1c0781d23d963691fadb2c181654b9ee7becbcca
Frode Nilsen <frode.nilsen@ruter.no>
2022-08-18 10:22:37 +0200

    Demonstrate how Navigate to Decalaration works in TestInLibModule but not in TestInAppModule in Android Studio Chipmunk

---
+++ app/src/androidTest/java/com/example/deps/TestInAppModule.kt
@@ -0,0 +1,22 @@
+package com.example.deps
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class TestInAppModule {
+    @Test
+    fun useAppContext() {
+        val q = com.example.lib2.R.layout.kaz
+    }
+}
